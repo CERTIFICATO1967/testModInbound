205f75f20c5c8e1ada94e677576746ea09c4702d
danlanzi <danilo.lanzi@eng.it>
2021-09-01 12:08:11 +0200

    Compilato

--- jaxws-opsc-services/src/main/java/it/telecomitalia/soa/trcs/gateway/infobus/commons/package-info.java
+++ jaxws-opsc-services/src/main/java/it/telecomitalia/soa/trcs/gateway/infobus/commons/package-info.java
@@ -2,7 +2,7 @@
 // Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.3.0 
 // Vedere <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2021.08.02 alle 11:23:32 AM CEST 
+// Generato il: 2021.09.01 alle 12:07:18 PM CEST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://telecomitalia.it/SOA/TRCS/Gateway/Infobus/Commons", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
