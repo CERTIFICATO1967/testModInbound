167773e47a5770a91bd71fee9353071b951e624b
danlanzi <danilo.lanzi@eng.it>
2021-09-01 12:10:04 +0200

    rimossi file compilati

--- jaxws-opsc-services/src/main/java/it/telecomitalia/soa/soap/soapheader/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.3.0 
-// Vedere <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2021.09.01 alle 12:07:18 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://telecomitalia.it/SOA/SOAP/SOAPHeader", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package it.telecomitalia.soa.soap.soapheader;
