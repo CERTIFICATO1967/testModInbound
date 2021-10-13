282d108108d382fa4128146011ae61abdcd6cdab
S226294 <raffaele.capasso@eng.it>
2021-10-13 17:12:28 +0200

    gestione combine

--- jaxws-recharge-services/src/main/java/it/telecomitalia/soa/trcs/gateway/recharge/package-info.java
+++ jaxws-recharge-services/src/main/java/it/telecomitalia/soa/trcs/gateway/recharge/package-info.java
@@ -2,7 +2,7 @@
 // Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.3.0 
 // Vedere <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2021.10.13 alle 05:08:21 PM CEST 
+// Generato il: 2021.10.13 alle 05:11:06 PM CEST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://telecomitalia.it/SOA/TRCS/Gateway", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
