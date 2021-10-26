7cf8e733ec9c69bf54822c26b84d0498259bc212
CERTIFICATO1967 <93213195+CERTIFICATO1967@users.noreply.github.com>
2021-10-26 18:34:57 +0200

    Update KafkaProducerConfig.java
    TEST X KAFKA

--- trcs-kafka-inbound-adapter/src/main/java/it/telecomitalia/trcs/middleware/kafka/inbound/config/KafkaProducerConfig.java
+++ trcs-kafka-inbound-adapter/src/main/java/it/telecomitalia/trcs/middleware/kafka/inbound/config/KafkaProducerConfig.java
@@ -18,7 +18,7 @@ public class KafkaProducerConfig {
 	@Value("${kafka.bootstrap-servers}")
 	private String bootstrapServers;
 
-	/*@Value("${kafka.security.protocol:SASL_SSL}")
+	@Value("${kafka.security.protocol:SASL_SSL}")
 	private String securuty_protocol;
 	@Value("${kafka.sasl.jaas.config:org.apache.kafka.common.security.plain.PlainLoginModule   required username='LTVL5DA226U3NK6L'   password='598mOQXYS9vtO5ZgeXr1uYU19bRpTl4F7xhbnZ3MbpN3+tLnOTAXUwCuXZh5ZIgL';}")
 	private String sasl_jassconfig;
@@ -27,7 +27,7 @@ public class KafkaProducerConfig {
 	@Value("${kafka.client.dns.lookup:use_all_dns_ips}")
 	private String client_dns_lookup;
 	@Value("${kafka.acks:all}")
-	private String acks;*/
+	private String acks;
 	
 	/*@Value("${kafka.schema.registry.url: https://{{ SR_ENDPOINT }}}")
 	private String schema_registry_url;
@@ -48,11 +48,11 @@ public class KafkaProducerConfig {
         configProps.put(
           ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, 
           StringSerializer.class);
-       /* configProps.put("security.protocol",securuty_protocol);
+        configProps.put("security.protocol",securuty_protocol);
         configProps.put("sasl.jaas.config",sasl_jassconfig);
         configProps.put("sasl.mechanism",sasl_mechanism);
         configProps.put("client.dns.lookup",client_dns_lookup);
-        configProps.put("acks",acks);*/
+        configProps.put("acks",acks);
         
        /* configProps.put("schema.registry.url",schema_registry_url);
         configProps.put("basic.auth.credentials.source",basic_auth_credentials_source);
